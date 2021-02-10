6051577fb9aa947b9333884df9cd2689c5fc1dc3
Richard Doucet <admin@zebrai17.com>
2021-02-10 06:24:40 -0800

    Before We Start

---
+++ CleanCoders/GateKeeper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanCoders
+{
+    public class GateKeeper
+    {
+        public User LoggerInUser { get; private set; }
+        public void SetCurrentUser(User loggedInUser)
+        {
+            LoggerInUser = loggedInUser;
+        }
+    }
+}
