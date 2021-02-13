eeb20ce9ee7746c51c310dd28e7633aa4b60937e
Richard Doucet <admin@zebrai17.com>
2021-02-12 19:27:47 -0800

    Episode5

--- CleanCoders.WebServer.UnitTests/SocketServerTests.cs
+++ CleanCoders.WebServer.UnitTests/SocketServerTests.cs
@@ -1,67 +1,243 @@
 using NUnit.Framework;
+using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 using System.Threading;
 
 namespace CleanCoders.WebServer.UnitTests
 {
+    [TestFixture]
     public class SocketServerTests
     {
-        private int _port;
-        private FakeSocketService _service;
-        private SocketServer _server;
-
-        public class FakeSocketService : ISocketService
+        class WithClosingSocketService
         {
-            public int Connections { get; private set; }
+            private int _port;
+            private ClosingSocketService _service;
+            private SocketServer _server;
 
-            public void Serve(Socket socket)
+            public class ClosingSocketService : TestSocketService
             {
-                Connections++;
-                socket.Close();
+                public int Connections { get; private set; }
+
+                protected override void DoService(Socket socket)
+                {
+                    Connections++;
+                }
+            }
+
+            [SetUp]
+            public void SetUp()
+            {
+                _port = 8042;
+                _service = new ClosingSocketService();
+                _server = new SocketServer(_port, _service);
+            }
+
+            [TearDown]
+            public void TearDown()
+            {
+                _server.Stop();
+            }
+
+            [Test]
+            public void Instansiate()
+            {
+                Assert.AreEqual(_port, _server.Port);
+                Assert.AreEqual(_service, _server.Service);
+            }
+
+            [Test]
+            public void CanStartAndStopServer()
+            {
+                _server.Start();
+                Assert.True(_server.IsRunning);
+                _server.Stop();
+                Assert.False(_server.IsRunning);
+            }
+
+            [Test]
+            public void acceptsAnIncommingConntection()
+            {
+                _server.Start();
+                var socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+                socket.Connect(IPAddress.Parse("127.0.0.1"), _port);
+                lock (_service)
+                {
+                    Monitor.Wait(_service);
+                }
+
+                _server.Stop();
+
+                Assert.AreEqual(1, _service.Connections);
+            }
+
+            [Test]
+            public void acceptsMultipleIncommingConntections()
+            {
+                _server.Start();
+                var socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+                var socket2 = new Socket(SocketType.Stream, ProtocolType.Tcp);
+                socket.Connect(IPAddress.Parse("127.0.0.1"), _port);
+                lock (_service)
+                {
+                    Monitor.Wait(_service);
+                }
+
+                socket2.Connect(IPAddress.Parse("127.0.0.1"), _port);
+                lock (_service)
+                {
+                    Monitor.Wait(_service);
+                }
+
+                _server.Stop();
+
+                Assert.AreEqual(2, _service.Connections);
             }
         }
-    
 
-        [SetUp]
-        public void SetUp()
-        {
-            _port = 8042;
-            _service = new FakeSocketService();
-            _server = new SocketServer(_port, _service);
+
+        public class WithReadingService {
+
+            private int _port;
+            private ReadingSocketService _service;
+            private SocketServer _server;
+            
+            public class ReadingSocketService : TestSocketService
+            {
+                public string Message { get; set; }
+
+
+                protected override void DoService(Socket socket)
+                {
+                    var buffer = new byte[100];
+                    var length = socket.Receive(buffer);
+                    Message = Encoding.UTF8.GetString(buffer).Substring(0, length);
+                }
+            }
+
+            [SetUp]
+            public void SetUp()
+            {
+                _port = 8042;
+                _service = new ReadingSocketService();
+                _server = new SocketServer(_port, _service);
+            }
+
+            [TearDown]
+            public void TearDown()
+            {
+                _server.Stop();
+            }
+
+            [Test]
+            public void CanSendAndRecievedData()
+            {
+                var _server = new SocketServer(_port, _service);
+                _server.Start();
+
+                var socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+                socket.Connect(IPAddress.Parse("127.0.0.1"), _port);
+                socket.Send(Encoding.UTF8.GetBytes("Hello\n"));
+
+                lock (_service)
+                {
+                    Monitor.Wait(_service);
+                }
+
+                _server.Stop();
+
+                Assert.AreEqual("Hello\n", _service.Message);
+                _server.Stop();
+            }
         }
 
-        [TearDown]
-        public void TearDown()
+        public class WithEchoService
         {
-            _server.Stop();
+
+            private int _port;
+            private EchoSocketService _service;
+            private SocketServer _server;
+
+            public class EchoSocketService : TestSocketService
+            {
+                public byte[] _buffer;
+
+
+                protected override void DoService(Socket socket)
+                {
+                    ReadFrom(socket);
+                    WriteTo(socket);
+                }
+
+                private void WriteTo(Socket socket)
+                {
+                    socket.Send(_buffer);
+                }
+
+                private void ReadFrom(Socket socket)
+                {
+                    var buffer = new byte[100];
+                    var length = socket.Receive(buffer);
+                    
+                    _buffer = new byte[length];
+                    Array.Copy(buffer, _buffer, length);
+                }
+            }
+
+            [SetUp]
+            public void SetUp()
+            {
+                _port = 8042;
+                _service = new EchoSocketService();
+                _server = new SocketServer(_port, _service);
+            }
+
+            [TearDown]
+            public void TearDown()
+            {
+                _server.Stop();
+            }
+
+            [Test]
+            public void CanSendAndRecievedData()
+            {
+                var _server = new SocketServer(_port, _service);
+                _server.Start();
+
+                var socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+                socket.Connect(IPAddress.Parse("127.0.0.1"), _port);
+                socket.Send(Encoding.UTF8.GetBytes("Hello\n"));
+
+                lock (_service)
+                {
+                    Monitor.Wait(_service);
+                }
+
+                var buffer = new byte[100];
+                var length = socket.Receive(buffer);
+                var message = Encoding.UTF8.GetString(buffer).Substring(0, length);
+
+                _server.Stop();
+
+                Assert.AreEqual("Hello\n", message);
+                _server.Stop();
+            }
         }
 
-        [Test]
-        public void Instansiate()
+        public abstract class TestSocketService : ISocketService
         {
-            Assert.AreEqual(_port, _server.Port);
-            Assert.AreEqual(_service, _server.Service);
-        }
+            public void Serve(Socket socket)
+            {
+                DoService(socket);
 
-        [Test]
-        public void CanStartAndStopServer()
-        {
-            _server.Start();
-            Assert.True(_server.IsRunning);
-            _server.Stop();
-            Assert.False(_server.IsRunning);
-        }
-
-        [Test]
-        public void acceptsAnIncommingConntection()
-        {
-            _server.Start();
-            var socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
-            socket.Connect(IPAddress.Parse("127.0.0.1"), _port);
-            Thread.Sleep(1); //Hack to setup order of operation
-
-            Assert.AreEqual(1, _service.Connections);
+                socket.Close();
+                lock (this)
+                {
+                    Monitor.Pulse(this);
+                }
+            }
+            protected abstract void DoService(Socket socket);
         }
     }
 }
--- CleanCoders.WebServer/SocketServer.cs
+++ CleanCoders.WebServer/SocketServer.cs
@@ -1,37 +1,46 @@
 ﻿using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CleanCoders.WebServer
 {
-    public class SocketServer
+    public class SocketServer 
     {
         private TcpListener _socketServer;
         public ISocketService Service { get; }
         public int Port { get; }
-        public bool? IsRunning { get; private  set; }
+        public bool IsRunning { get; private  set; }
+        private CancellationTokenSource _tokenSource;
 
         public SocketServer(int port, ISocketService service)
         {
             Service = service;
             Port = port;
             _socketServer = new TcpListener(IPAddress.Parse("127.0.0.1"), Port);
-
+            _tokenSource =  new CancellationTokenSource();
         }
 
         public void Start()
         {
             _socketServer.Start();
-
-            Task.Run(() => Service.Serve(_socketServer.AcceptSocket()));
-
             IsRunning = true;
+
+            Task.Run(() =>
+            {
+                while (IsRunning)
+                {
+                    var socket = _socketServer.AcceptSocket();
+                    Task.Run(() => Service.Serve(socket), _tokenSource.Token);
+                }
+            }, _tokenSource.Token);
         }
         
         public void Stop()
         {
-            _socketServer.Stop();
             IsRunning = false;
+            _socketServer.Stop();
+            _tokenSource.Cancel();
         }
     }
 }
